41f8a33b59f11efa428cde824309a49120fab939
Indrajeet Yaduvanshi <149613654+yaduvanshiindrajeet@users.noreply.github.com>
2026-06-03 06:49:11 +0000

    for loop complete

---
+++ Loop/forLoopExamples.dart
@@ -0,0 +1,49 @@
+import 'dart:io';
+// void main()
+// {
+//     int i;
+//     for(i = 1; i <= 10; i++)
+//     {
+//         print(i);
+//     }
+// }
+
+
+// print even numers between 1 to 20
+
+// void main()
+// {
+//     int i;
+//     for(i = 1; i <= 20; i++){
+//         if(i%2 == 0){
+//             print(i);
+//         }
+//     }
+// }
+
+
+// print multiplication table of any number;
+
+// void main()
+// {
+//     print("Enter a number: ");
+//     int num = int.parse(stdin.readLineSync()!);
+//     int i;
+
+//     for(i = 1; i <=10; i++)
+//     {
+//         print("$num X $i = ${num*i}");
+//     }
+// }
+
+
+// sum of five digits
+
+void main()
+{
+    int num, sum, rem;
+    print("Enter a five digits number: ");
+    num = int.parse(stdin.readLineSync()!);
+
+    for()
+}
