40e820db53cd9e149860611a6215e7c1f46c3ede
Indrajeet Yaduvanshi <149613654+yaduvanshiindrajeet@users.noreply.github.com>
2026-06-03 06:48:50 +0000

    for loo[

--- DataType/dartList.dart
+++ DataType/dartList.dart
@@ -1,9 +1,17 @@
+// void main()
+// {
+//     List? emp = List.filled(5, null, growable:false);
+//     emp[0] = 'Emp ID:';
+//     emp[1] = 'Emp Name:';
+//     emp[2] = 'Emp Designations:';
+//     print(emp);
+//     print(emp[2]);
+// }
+
 void main()
 {
-    List? emp = List.filled(5, null, growable:false);
-    emp[0] = 'Emp ID:';
-    emp[1] = 'Emp Name:';
-    emp[2] = 'Emp Designation:';
-    print(emp);
-    print(emp[1]);
+    List num = [1,2,3];
+    List<String> name = ['Aman Kumar', 'Sandeep'];
+    print(num);
+    print(name);
 }
